82f85b4987c9d404717eaceec97a7468dfb46d39
victorH-Dev <vhshugtsfull@gmail.com>
2024-11-16 13:33:42 -0300

    first commit

---
+++ js/formulario.js
@@ -0,0 +1,5 @@
+// Exemplo de Validação de Formulário com JavaScript
+document.querySelector('form').onsubmit = function(e) {
+    // Validação dos campos aqui
+    // Se a validação falhar, use e.preventDefault() para impedir o envio do formulário
+  };
