bd158bd3773983ccac30f9f87eae007c3b5fd29a
Victor H <vhshugtsfull@gmail.com>
2025-12-10 20:32:05 -0300

    Move project to repository root; remove nested folder

--- quiroz-main/js/formulario.js
+++
@@ -1,5 +0,0 @@
-// Exemplo de Validação de Formulário com JavaScript
-document.querySelector('form').onsubmit = function(e) {
-    // Validação dos campos aqui
-    // Se a validação falhar, use e.preventDefault() para impedir o envio do formulário
-  };
